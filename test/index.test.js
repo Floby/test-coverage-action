d749f731e86702ef52fbbf0d846eeacf4d827a53
Florent Jaby <florent.jaby@gmail.com>
2022-08-19 15:16:21 +0200

    Revert "fixup test"
    This reverts commit defe4365e3bcb818e5f8bfc1316a0f34839cedd4.

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,4 @@ describe('default function', () => {
   it('returns true', () => {
     expect(fn()).toEqual(true)
   })
-  describe('when flag', () => {
-    it('returns false', () => {
-      expect(fn(true)).toEqual(false)
-    })
-  })
 })
